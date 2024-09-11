e108a49fcff6b3f18824153ae1ef985e93de5355
admin <admin@gmail.com>
2024-09-11 14:44:32 +0530

    Initial commit of static website

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login form submitted!');
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Signup form submitted!');
+});
+
